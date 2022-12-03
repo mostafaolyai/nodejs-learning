ee4897f0872e47aa34c4649a900b76572dd750c3
mostafa <amarali2008@gmail.com>
2022-12-03 15:19:18 +0330

    routing more

---
+++ 9.enhance-app/controllers/shop.js
@@ -0,0 +1,44 @@
+const Product = require('../models/product');
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/cart', {
+      prods: products,
+      pageTitle: 'Cart',
+      path: '/cart',
+    });
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/checkout', {
+      prods: products,
+      pageTitle: 'Checkout',
+      path: '/checkout',
+    });
+  });
+};
