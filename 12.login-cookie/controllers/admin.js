90d1620f2ad5542863000eea88873aced058b519
mostafa <amarali2008@gmail.com>
2022-12-11 16:28:20 +0330

    signup

--- 12.login-cookie/controllers/admin.js
+++ 12.login-cookie/controllers/admin.js
@@ -1,6 +1,12 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
+  
+  //it's not the best way
+  // if(!req.session.isLoggedIn){
+  //   return res.redirect('login')
+  // }
+
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
--- 12.login-cookie/controllers/auth.js
+++ 12.login-cookie/controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/user')
+
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
@@ -9,13 +11,56 @@ exports.getLogin = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
     // req.isLoggedIn = true;
     // res.setHeader('Set-Cookie', 'loggedin=true MAX-AGE=10,hhtponly , secure, ')//can see in browser/snippet/application
-    req.session.isLoggedIn = true;
-    res.redirect('/');
+    
+    const email = req.body.email;
+    const password = req.body.password
+    
+    User.findOne({email})
+      .then(user => {
+        if(!user) res.redirect('/login')
+        req.session.isLoggedIn = true;
+        req.session.user = user;
+
+        return req.session.save(err => {
+          res.redirect('/');
+        })
+        
+      })
+      .catch(err => console.log(err))
+    
   };
 
-  exports.postLogout = (req, res, next) => {
+exports.postLogout = (req, res, next) => {
       req.session.destroy((err) => {
         console.log(err)
         res.redirect('/');
       })
-    };
+  };
+
+  exports.getSignup = (req, res, next) => {
+    res.render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      isAuthenticated: req.session.isLoggedIn 
+    });
+  };
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password
+
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if(userDoc) return res.redirect('/signup')
+
+      const user = new User({
+        email: email,
+        password: password,
+        cart: { items: [] }
+      })
+      return user.save()
+    })
+    .then(user => {
+      return res.redirect('/')
+    })
+    .catch(err => console.log(err))
+}
